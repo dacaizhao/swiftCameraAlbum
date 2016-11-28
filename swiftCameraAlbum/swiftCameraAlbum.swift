85f4ab8d6e088a01028901fc43debdc86309721a
app@yangland.com <327532817@qq.com>
2016-11-28 14:27:47 +0800

    配置iOS版本

---
+++ swiftCameraAlbum/swiftCameraAlbum.swift
@@ -0,0 +1,13 @@
+//
+//  swiftCameraAlbum.swift
+//  swiftCameraAlbum
+//
+//  Created by point on 2016/11/28.
+//  Copyright © 2016年 dacai. All rights reserved.
+//
+
+import UIKit
+
+class swiftCameraAlbum: NSObject {
+
+}
