aee7c26558710b35db9374bd479ab39b7862c7ea
app@yangland.com <327532817@qq.com>
2016-11-29 21:09:02 +0800

    修改结构

---
+++ swiftCameraAlbum/TakePic/TakePicListCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TakePicListCollectionViewCell.swift
+//  swiftCameraAlbum
+//
+//  Created by point on 2016/11/28.
+//  Copyright © 2016年 dacai. All rights reserved.
+//
+
+import UIKit
+
+class TakePicListCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var img: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        img.backgroundColor = UIColor.red
+    }
+
+}
